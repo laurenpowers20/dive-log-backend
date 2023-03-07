9af408e05e22c8e31e764c661be8d48900e79cab
Lauren Powers <laurenpowers@Laurens-Air.lan>
2023-03-06 20:27:15 -0500

    mongoDB variable

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
 let MONGODB_URI =
-  process.env.PROD_MONGODB ||
-  "mongodb://127.0.0.1:27017/productsAuthenticationDatabase";
-
+  process.env.LOG_MONGODB ||
+  "mongodb://127.0.0.1:27017/logsAuthenticationDatabase";
 
 //  https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
 mongoose.set("returnOriginal", false);
